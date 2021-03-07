97aa809d5ceefebd23c31d0996b1aa034ddb3ed0
Jepherson <jephcescon@hotmail.com>
2021-03-07 15:55:05 -0300

    tela de login esboço

--- app/src/main/java/com/example/cinequiz/MainActivity.kt
+++ app/src/main/java/com/example/cinequiz/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.cinequiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        /*
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
+
+        getSupportActionBar()?.hide(); // hide the title bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
+        */
         setContentView(R.layout.activity_main)
     }
 }
