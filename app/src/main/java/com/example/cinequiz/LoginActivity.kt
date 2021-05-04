3818a06ea65ea0e119e8cdcdd5a33aef5a27aba9
Arthur-Otero <arthurotero2014@gmail.com>
2021-05-03 21:34:36 -0300

    Trocando as cores

--- app/src/main/java/com/example/cinequiz/LoginActivity.kt
+++ app/src/main/java/com/example/cinequiz/LoginActivity.kt
@@ -29,11 +29,6 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
-//
-//        supportActionBar?.hide(); // hide the title bar
-//        this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-//                WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
         setContentView(R.layout.activity_login)
 
         firebaseAuth = FirebaseAuth.getInstance()
