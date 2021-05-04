22100b24139524be0a55a6fb019cc40ed22e4d79
Pauloo98 <pauloo98@gmail.com>
2021-05-03 21:20:50 -0300

    Inserido o Login Facebook

--- app/src/main/java/com/example/cinequiz/LoginActivity.kt
+++ app/src/main/java/com/example/cinequiz/LoginActivity.kt
@@ -10,10 +10,18 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.facebook.AccessToken
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.Login
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 
@@ -25,7 +33,10 @@ class LoginActivity : AppCompatActivity() {
     lateinit var firebaseAuth: FirebaseAuth
     lateinit var googleSignInClient: GoogleSignInClient
 
-    private val btnGoogle by lazy { findViewById<Button>(R.id.btnGoogle) }
+    private lateinit var callbackManager: CallbackManager
+    private var loginManager = LoginManager.getInstance()
+
+    private val btnFacebook by lazy { findViewById<Button>(R.id.btnFacebook) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,6 +48,7 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         firebaseAuth = FirebaseAuth.getInstance()
+        callbackManager = CallbackManager.Factory.create()
 
         val googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken(getString(R.string.default_web_client_id))
@@ -64,10 +76,15 @@ class LoginActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        btnFacebook.setOnClickListener {
+            signFacebook()
+        }
+
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
+        callbackManager.onActivityResult(requestCode, resultCode, data)
 
         if (requestCode == 200) {
             val task = GoogleSignIn.getSignedInAccountFromIntent(data)
@@ -104,4 +121,40 @@ class LoginActivity : AppCompatActivity() {
         val signInIntent = googleSignInClient.signInIntent
         startActivityForResult(signInIntent,200)
     }
+
+    private fun signFacebook(){
+        loginManager.logInWithReadPermissions(this, arrayListOf("email","public_profile"))
+        loginManager.registerCallback(callbackManager, object: FacebookCallback<LoginResult>{
+            override fun onSuccess(result: LoginResult?) {
+                result?.let { firebaseAuthWithFacebook(it.accessToken) }
+
+            }
+
+            override fun onCancel() {
+                Log.d("Facebook", "deu erro no login")
+
+            }
+
+            override fun onError(error: FacebookException?) {
+                Log.d("Facebook", "deu erro no login")
+            }
+
+        })
+    }
+
+    private fun firebaseAuthWithFacebook(idToken: AccessToken) {
+        val credential = FacebookAuthProvider.getCredential(idToken.token)
+        firebaseAuth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("GoogleSign", "signInWithCredential:success")
+                    val user = firebaseAuth.currentUser
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("GoogleSign", "signInWithCredential:failure", task.exception)
+                }
+            }
+    }
+
 }
